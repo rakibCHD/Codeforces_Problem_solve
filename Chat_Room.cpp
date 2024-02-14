b6e658a7df403349716bca107766f53e3b2f9f8e
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-15 01:26:05 +0600

    Add files via upload

---
+++ Chat_Room.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string hello = "hello";
+    int j = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == hello[j]) {
+            ++j;
+        }
+        if (j == hello.length()) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+    return 0;
+}
