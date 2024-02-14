ce7fe0f67c9b9a8684c024d98db376606bee8209
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-15 01:26:22 +0600

    Add files via upload

---
+++ Lucky_Number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_lucky(int n) {
+    while (n > 0) {
+        int digit = n % 10;
+        if (digit != 4 && digit != 7) {
+            return false;
+        }
+        n /= 10;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0 && is_lucky(i)) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+    return 0;
+}
