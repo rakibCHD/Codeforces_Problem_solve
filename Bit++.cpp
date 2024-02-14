dbf05165e1a5facf87ade13e9116c6623d6f414e
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-14 19:27:37 +0600

    Add files via upload

---
+++ Bit++.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n,x=0;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string statement;
+        cin >> statement;
+
+        if (statement[0] == '+' && statement[1] == '+') {
+            ++x;
+        }
+        else if(statement[1] == '+' && statement[2] == '+'){
+            x++;
+        }
+        else if(statement[1] == '-' && statement[2] == '-') {
+            x--;
+        }
+        else if(statement[0] == '-' && statement[1] == '-') {
+            --x;}
+        else{
+            return 0;}
+    }
+    cout << x << endl;
+    return 0;
+}
