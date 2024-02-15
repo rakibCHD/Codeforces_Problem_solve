948849f39e0da588be1adf4ef9ea40f23eafd8fb
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-16 00:22:07 +0600

    Add files via upload

---
+++ Drinks.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> percentages(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> percentages[i];
+    }
+
+    double total_volume = 0;
+    for (int i = 0; i < n; ++i) {
+        total_volume += percentages[i] / 100.0;
+    }
+
+    double cocktail_percentage = (total_volume / n) * 100;
+
+    cout << setprecision(10) << cocktail_percentage << endl;
+
+    return 0;
+}
