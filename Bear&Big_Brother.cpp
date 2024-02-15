be588afcee45bec30450486bc46c6aff626b1346
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-15 22:20:10 +0600

    Add files via upload

---
+++ Bear&Big_Brother.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int years = 0;
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+
+    cout << years << endl;
+
+    return 0;
+}
