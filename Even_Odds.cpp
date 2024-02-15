dac6afc3f34d586eec7e709f92e3fd3f8ea341a7
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-15 22:21:42 +0600

    Add files via upload

---
+++ Even_Odds.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long findNumber(long long n, long long k) {
+    if (k <= (n + 1) / 2) {
+        return 2 * k - 1;
+    } else {
+        return 2 * (k - (n + 1) / 2);
+    }
+}
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    cout << findNumber(n, k) << endl;
+
+    return 0;
+}
