e4506caabf87b5b789d51df71d23b13dd53df925
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-16 00:26:12 +0600

    Add files via upload

---
+++ Vanya&Fence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, h;
+    cin >> n >> h;
+
+    vector<int> heights(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> heights[i];
+    }
+
+    int width = 0;
+    for (int i = 0; i < n; ++i) {
+        if (heights[i] > h) {
+            width += 2;
+        } else {
+            width += 1;
+        }
+    }
+
+    cout << width << endl;
+
+    return 0;
+}
