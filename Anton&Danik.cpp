e59cc5cc6a943a6509635559613f0c3e2c248242
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-15 22:19:49 +0600

    Add files via upload

---
+++ Anton&Danik.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int main() {
+    int n,A=0,D=0;
+    string input;
+    cin>>n>>input;
+        for(int i=0;i<n;i++)
+        {
+            if(input[i]=='A' || input[i]=='a')
+                A++;
+            if(input[i]=='D' || input[i]=='d')
+                D++;
+        }
+    if(A>D)
+        cout<<"Anton"<<endl;
+    if(A<D)
+        cout<<"Danik"<<endl;
+    if(A==D)
+        cout<<"Friendship"<<endl;
+
+    return 0;
+}
