20a15f713cf4ee2e6d1311b8008d4e2f2dcc1a33
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-17 00:24:08 +0600

    Add files via upload

---
+++ Twins.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> coins(n);
+    int total = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> coins[i];
+        total += coins[i];
+    }
+
+    sort(coins.begin(), coins.end(), greater<int>());
+
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        sum += coins[i];
+        ++count;
+
+        if (sum > total - sum) {
+            break;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
