976d08e59d4722be7f8115cd2188b439e403dd03
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-17 00:23:14 +0600

    Add files via upload

---
+++ Plus_Minus.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    string result;
+
+    for (int i = 0; i < t; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a + b == c)
+            result += '+';
+        else
+            result += '-';
+    }
+
+    for (int i = 0; i < t; i++)
+        cout << result[i] << endl;
+
+    return 0;
+}
