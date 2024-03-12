a29ad738cec7afa71f1075f92c6237fc99dc2ce6
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-13 02:52:58 +0600

    Add files via upload

---
+++ Taxi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int groups[5] = {0};
+
+    for (int i = 0; i < n; ++i) {
+        int s;
+        cin >> s;
+        groups[s]++;
+    }
+
+    int taxis = groups[4] + groups[3] + (groups[2] + 1) / 2;
+    groups[1] -= groups[3];
+    if (groups[2] % 2 == 1)
+        groups[1] -= 2;
+    if (groups[1] > 0)
+        taxis += (groups[1] + 3) / 4;
+
+    cout << taxis << endl;
+
+    return 0;
+}
