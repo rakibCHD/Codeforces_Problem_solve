5946d9f322c6b082839e8bdb841eaa155e5279de
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-24 19:24:47 +0500

    Add files via upload

---
+++ Beautiful_Matrix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int matrix[5][5];
+    int moves = 0;
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int row, col;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            if (matrix[i][j] == 1) {
+                row = i;
+                col = j;
+                break;
+            }
+        }
+    }
+
+    moves = abs(row - 2) + abs(col - 2);
+
+    cout << moves << endl;
+
+    return 0;
+}
