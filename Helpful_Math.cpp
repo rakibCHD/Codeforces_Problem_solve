eddeef082335f1b44614b5767fe1aba3b5390b7f
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-24 19:25:03 +0500

    Add files via upload

---
+++ Helpful_Math.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] != '+') {
+            cout << s[i];
+            if (i != s.length() - 1)
+                cout << '+';
+        }
+    }
+    cout << endl;
+    return 0;
+}
